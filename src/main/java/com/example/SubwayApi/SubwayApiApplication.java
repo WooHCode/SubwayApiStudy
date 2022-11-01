43598f920a71bb69800618851c07385fb2e57875
WooHCode <gusdn526@naver.com>
2022-11-01 18:46:35 +0900

    Initial commit

---
+++ src/main/java/com/example/SubwayApi/SubwayApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SubwayApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwayApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SubwayApi/controller/LocationController.java
@@ -0,0 +1,21 @@
+package com.example.SubwayApi.controller;
+
+import com.example.SubwayApi.service.subwayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+    @Autowired
+    private subwayService subwayService;
+
+    @GetMapping("/location") // 현재 가장 최근 위치를 불러온다, 어느 역인지, 몇호선인지
+    public String getLocation(
+            @RequestParam String station,@RequestParam String trainNumber
+    ){
+      return subwayService.getLocation(station, trainNumber);
+
+    }
+}
